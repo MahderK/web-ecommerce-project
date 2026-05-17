70053335ea46e93fe479dba9d8e4436c429d0ff8
maki-ni <nehemiahelias14@gmail.com>
2026-05-17 21:52:37 +0300

    built a simple navbar

---
+++ includes/navbar.php
@@ -0,0 +1,12 @@
+<header class="header">
+    <div class="navbar container">
+        <div class="logo">
+            <a href="index.php">Toiletries</a>
+        </div>
+        <nav class="nav-links">
+            <a href="index.php">Home</a>
+            <a href="products.php">Products</a>
+            
+        </nav>
+    </div>
+</header>
