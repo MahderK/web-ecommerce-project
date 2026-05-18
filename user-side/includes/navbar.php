8bfb78efcf045e52d6c67ee9523eaae7bb3fa002
MahderK <mahderkassaw@gmail.com>
2026-05-18 10:12:42 +0300

    fix: resolve navigation path conflicts in navbar icons

--- user-side/includes/navbar.php
+++ user-side/includes/navbar.php
@@ -12,17 +12,10 @@
 
     <div class="icons">
       <span><i class="fa-solid fa-magnifying-glass"></i></span>
-<<<<<<< HEAD:includes/navbar.php
-      <a href="../login.php">
-      <span><i class="fa-regular fa-user"></i></span>
-      </a>
-      <a href="../cart.php">
-=======
       <a href="login.php">
       <span><i class="fa-regular fa-user"></i></span>
       </a>
       <a href="cart.php">
->>>>>>> e2e5a626dd68460d33c5793236a8c381c56c83df:user-side/includes/navbar.php
       <span><i class="fa-solid fa-cart-shopping"></i></span>
 
       </a>
