4844358013902954a274dd1e544a3f39c824b308
MahderK <mahderkassaw@gmail.com>
2026-06-23 05:51:50 +0300

    fix: change reference of banner.php button to correct one.

--- user-side/sections/banners.php
+++ user-side/sections/banners.php
@@ -5,7 +5,7 @@
       <div>
         <small>Big Sale On Indoor</small>
         <h2>Indoor Plants</h2>
-        <a href="#">Shop Now →</a>
+        <a href="products.php">Shop Now →</a>
       </div>
 
       <img src="https://images.unsplash.com/photo-1485955900006-10f4d324d411?q=80&w=1000&auto=format&fit=crop">
